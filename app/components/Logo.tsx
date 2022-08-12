6e902d3882023faed5fd2dfa12864f7cd8d27df5
Alexandru Adam <dm_alexandru@yahoo.com>
2022-08-12 21:19:47 +0300

    Fixed bug for logo not showing on center.

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -5,15 +5,17 @@ const LOGO_SIZE = 78;
 
 export const Logo: React.FC = () => {
   return (
-    <Link to="/" prefetch="intent">
-      <div className="order-2 cursor-pointer hover:opacity-70 lg:order-1">
-        <img
-          src="https://cdn.shopify.com/s/files/1/0598/9367/8278/files/logo_transparent_156x.png?v=1649422810"
-          alt="logo"
-          width={LOGO_SIZE}
-          height={LOGO_SIZE}
-        />
-      </div>
+    <Link
+      to="/"
+      prefetch="intent"
+      className="order-2 cursor-pointer hover:opacity-70 lg:order-1"
+    >
+      <img
+        src="https://cdn.shopify.com/s/files/1/0598/9367/8278/files/logo_transparent_156x.png?v=1649422810"
+        alt="logo"
+        width={LOGO_SIZE}
+        height={LOGO_SIZE}
+      />
     </Link>
   );
 };
