f827dd4cb3f056e9c35beb837291525e33ad3dfb
Alexandru Adam <dm_alexandru@yahoo.com>
2022-11-18 09:07:55 +0200

    Improve animation for ScrollToTop

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { IoChevronUpOutline } from "react-icons/io5";
 import { FaSquareFull } from "react-icons/fa";
-import { Fade } from "./shared/Fade";
+import { motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 
 export const ScrollToTop: React.FC = () => {
   const [show, setShow] = useState(false);
@@ -21,18 +22,25 @@ export const ScrollToTop: React.FC = () => {
     };
   });
   return (
-    <Fade show={show}>
-      <div
-        className="fixed bottom-[60px] right-[20px] z-40 flex h-[42px] w-[26px] cursor-pointer flex-col items-center justify-center bg-primary text-white transition duration-300 ease-in-out "
-        onClick={() => {
-          window.scrollTo({ top: 0, behavior: "smooth" });
-        }}
-      >
-        <IoChevronUpOutline size={16} className="mt-[-5px]" />
-        <FaSquareFull size="3px" />
-        <FaSquareFull size="3px" className="mt-1" />
-        <FaSquareFull size="3px" className="mt-1" />
-      </div>
-    </Fade>
+    <AnimatePresence exitBeforeEnter>
+      {show && (
+        <motion.div
+          key="scroll-to-top"
+          transition={{ duration: 0.4 }}
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: 100, opacity: 0 }}
+          className="fixed bottom-[60px] right-[20px] z-40 flex h-[42px] w-[26px] cursor-pointer flex-col items-center justify-center bg-primary text-white transition duration-300 ease-in-out "
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        >
+          <IoChevronUpOutline size={16} className="mt-[-5px]" />
+          <FaSquareFull size="3px" />
+          <FaSquareFull size="3px" className="mt-1" />
+          <FaSquareFull size="3px" className="mt-1" />
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
