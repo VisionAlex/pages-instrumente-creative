9e00a092b9a8bb1dd822ccde19497e952b78124f
Alexandru Adam <dm_alexandru@yahoo.com>
2022-12-10 22:44:20 +0200

    Add seo for faq page.

--- app/routes/serviciu-clienti.tsx
+++ app/routes/serviciu-clienti.tsx
@@ -1,5 +1,7 @@
+import type { HeadersFunction, MetaFunction } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
+import { motion } from "framer-motion";
 import React from "react";
 import { PageHeader } from "~/components/shared/PageHeader";
 import { PAGE_HANDLE } from "~/config";
@@ -13,25 +15,49 @@ type LoaderData = {
 
 export const loader = async () => {
   const page = await getPage({ handle: PAGE_HANDLE.CUSTOMER_SERVICE });
-  return json({
-    page: page.page,
-  });
+  return json(
+    {
+      page: page.page,
+    },
+    {
+      headers: {
+        "Cache-Control": "public, max-age=86400",
+      },
+    }
+  );
 };
 
 export const links = () => {
   return [{ rel: "stylesheet", href: pageStyles }];
 };
 
+export const headers: HeadersFunction = () => {
+  return {
+    "Cache-Control": "public, max-age=86400",
+  };
+};
+
+export const meta: MetaFunction = ({ data }) => {
+  return {
+    title: data.page.seo.title,
+    description: data.page.seo.description,
+  };
+};
 const CustomerService: React.FC = () => {
   const { page } = useLoaderData<LoaderData>();
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
       <PageHeader />
       <div
         className="page mx-auto max-w-7xl px-5 lg:px-8 xl:px-20"
         dangerouslySetInnerHTML={{ __html: page.body }}
       />
-    </div>
+    </motion.div>
   );
 };
 
