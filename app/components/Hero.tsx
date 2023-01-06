72d08e5422cec1e7124c654d144dd1379edbc476
Alexandru Adam <dm_alexandru@yahoo.com>
2023-01-06 08:33:53 +0200

    Slightly change hero image.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ import { LinkButton } from "./shared/LinkButton";
 
 export const Hero: React.FC = () => {
   return (
-    <div className="relative h-[110vw] max-h-[550px] w-full bg-[#fcf8f5]">
+    <div className="relative h-[110vw] max-h-[600px] w-full bg-[#fcf8f5]">
       <div className="absolute bottom-0 z-0 h-[100.7px] w-full bg-[#e8e2db]" />
       <div className="absolute z-20 flex w-full flex-col items-center justify-center gap-4 p-5 text-center">
         <h2 className="text-[8.6vw] leading-tight text-primary sm:mb-6 sm:text-header ">
@@ -21,8 +21,8 @@ export const Hero: React.FC = () => {
         />
       </div>
       <img
-        className="absolute left-0 right-0 z-10 mx-auto max-h-[550px]"
-        src="https://cdn.shopify.com/s/files/1/0598/9367/8278/files/Kit_slide_mobile_54330571-644e-43bb-a4b0-af2c0c503ff2.png?v=1651500342"
+        className="absolute left-0 right-0 z-10 mx-auto max-h-[600px]"
+        src="https://imagedelivery.net/_Qvu0ID0pPyVjDxZnl16ng/74e03979-b796-4bc4-28ae-d6d032709b00/gamma=0"
         alt="header"
       />
     </div>
