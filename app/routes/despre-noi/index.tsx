622925a3814f0e7abd646325d0cbe514e7122009
Alexandru Adam <dm_alexandru@yahoo.com>
2023-12-20 08:06:22 +0200

    Change image on the about page.

--- app/routes/despre-noi/index.tsx
+++ app/routes/despre-noi/index.tsx
@@ -8,11 +8,13 @@ import { useLoaderData } from "@remix-run/react";
 import React from "react";
 import { FadeIn } from "~/components/shared/FadeIn";
 import { PageHeader } from "~/components/shared/PageHeader";
-import { PAGE_HANDLE } from "~/config";
+import { PAGE_HANDLE, images } from "~/config";
 import type { GetPageQuery } from "~/generated/graphql";
 import { createSdk } from "~/graphqlWrapper";
 import { getPage } from "~/providers/pages/page";
 import pageStyles from "~/styles/page.css";
+
+const about = `${images.baseUrl}${images.images.about.src}`;
 
 type LoaderData = {
   page: NonNullable<GetPageQuery["page"]>;
@@ -48,8 +50,18 @@ const About: React.FC = () => {
   return (
     <FadeIn>
       <PageHeader />
+      <div className="aspect-w-3 aspect-h-2 w-full overflow-hidden">
+        <picture className="object-cover object-center transition">
+          <source srcSet={`${about}/w=1980`} media="(min-width: 1536px)" />
+          <source srcSet={`${about}/w=1536`} media="(min-width: 1280px)" />
+          <source srcSet={`${about}/w=1280`} media="(min-width: 1024px)" />
+          <source srcSet={`${about}/w=1024`} media="(min-width: 768px)" />
+          <source srcSet={`${about}/w=768`} media="(min-width:640px)" />
+          <img src={`${about}/w=500`} alt="Instrumente Creative" />
+        </picture>
+      </div>
       <div
-        className="page mx-auto max-w-7xl px-5 lg:px-8 xl:px-20"
+        className="page mx-auto mt-4 max-w-7xl px-5 lg:px-8 xl:px-20"
         dangerouslySetInnerHTML={{ __html: page.body }}
       />
     </FadeIn>
